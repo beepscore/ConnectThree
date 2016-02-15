d7acdf4452583649593a9f53937a8112bfd22b0b
beepscore <support@beepscore.com>
2016-02-14 16:59:23 -0800

    In MainActivity onButtonClick use tag to calculate row and column. Delete now unused method getButtonCoordinates.

--- app/src/main/java/com/beepscore/android/connectthree/MainActivity.java
+++ app/src/main/java/com/beepscore/android/connectthree/MainActivity.java
@@ -99,9 +99,10 @@ public class MainActivity extends AppCompatActivity {
     public void onButtonClick(View view) {
         ImageButton imageButton = (ImageButton) view;
 
-        Pair<Integer, Integer> buttonCoordinates = getButtonCoordinates(view);
-        int row = buttonCoordinates.first.intValue();
-        int column = buttonCoordinates.second.intValue();
+        int buttonIndex = Integer.parseInt(imageButton.getTag().toString());
+        // integer division truncates
+        int row = buttonIndex / numberOfRows;
+        int column = buttonIndex % numberOfRows;
 
         if (!GameController.isGameWon(mGameController.game.board)
                 && GameController.canMove(mGameController.game.board, row, column)) {
@@ -120,64 +121,6 @@ public class MainActivity extends AppCompatActivity {
                         mGameController.game.currentPlayer.playerId, Toast.LENGTH_SHORT).show();
             }
         }
-    }
-
-    /**
-     * @return a Pair of row, column
-     */
-    Pair<Integer, Integer> getButtonCoordinates(View view) {
-
-        int row = 0;
-        int column = 0;
-
-        switch(view.getId()) {
-            case R.id.imageButton00: {
-                row = 0;
-                column = 0;
-                break;
-            }
-            case R.id.imageButton01: {
-                row = 0;
-                column = 1;
-                break;
-            }
-            case R.id.imageButton02: {
-                row = 0;
-                column = 2;
-                break;
-            }
-            case R.id.imageButton10: {
-                row = 1;
-                column = 0;
-                break;
-            }
-            case R.id.imageButton11: {
-                row = 1;
-                column = 1;
-                break;
-            }
-            case R.id.imageButton12: {
-                row = 1;
-                column = 2;
-                break;
-            }
-            case R.id.imageButton20: {
-                row = 2;
-                column = 0;
-                break;
-            }
-            case R.id.imageButton21: {
-                row = 2;
-                column = 1;
-                break;
-            }
-            case R.id.imageButton22: {
-                row = 2;
-                column = 2;
-                break;
-            }
-        }
-        return Pair.create(row, column);
     }
 
 }
