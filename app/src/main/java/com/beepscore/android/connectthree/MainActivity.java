e792cc23701d2113339e5f80d547474e72ee4d0c
beepscore <support@beepscore.com>
2016-02-14 16:30:13 -0800

    Delete unused import.

--- app/src/main/java/com/beepscore/android/connectthree/MainActivity.java
+++ app/src/main/java/com/beepscore/android/connectthree/MainActivity.java
@@ -6,7 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.GridLayout;
 import android.widget.ImageButton;
 import android.widget.Toast;
